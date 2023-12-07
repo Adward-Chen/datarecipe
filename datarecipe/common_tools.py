d5ab7759b4a234d37af2b4de33c650d7ad97c718
Adward-Chen <HanfanC@outlook.com>
2023-12-07 16:32:02 +0800

    Update common_tools.py
    add df_to_csv function

--- datarecipe/common_tools.py
+++ datarecipe/common_tools.py
@@ -4,6 +4,7 @@ import pandas as pd
 import glob
 import os
 from tqdm import tqdm
+import chardet
 
 def send_email(subject: str, body: str, send_emial_address:str, send_emial_password:str, receive_email_address: str, smtp_address:str='smtp.feishu.cn', smtp_port:int=465):
     # For SMTP, the email and password
@@ -19,7 +20,7 @@ def send_email(subject: str, body: str, send_emial_address:str, send_emial_passw
         server.login(send_emial_address, PASSWORD)
         server.sendmail(send_emial_address, receive_email_address, msg.as_string())
 
-def local_to_df(path, partial_file_name, keep_file_name=False, sheet_num=1):
+def local_to_df(path, partial_file_name, skip_rows = None, keep_file_name=False, sheet_num=1):
     all_data = pd.DataFrame()
 
     # 获取匹配的文件列表，并为其添加进度条
@@ -28,9 +29,17 @@ def local_to_df(path, partial_file_name, keep_file_name=False, sheet_num=1):
     for file_name in tqdm(file_list, desc="Processing Files"):
         _, file_extension = os.path.splitext(file_name)
         file_extension = file_extension[1:]  # remove the leading dot
-
+        
+        # 识别文件编码类型
+        with open(file_name, 'rb') as file:
+            result = chardet.detect(file.read())
+            encoding = result['encoding']
+        
         if file_extension == "csv":
-            file_data = pd.read_csv(file_name)
+            if skip_rows:
+                file_data = pd.read_csv(file_name, encoding=encoding, skiprows=skip_rows)
+            else:
+                file_data = pd.read_csv(file_name, encoding=encoding)
         elif file_extension == "xlsx":
             file_data = pd.read_excel(file_name, sheet_name=sheet_num - 1)
         else:
@@ -42,7 +51,7 @@ def local_to_df(path, partial_file_name, keep_file_name=False, sheet_num=1):
         all_data = pd.concat([all_data, file_data])
 
     if all_data.empty:
-        print("No matching file in the path")
+        print("No matching file in the path or empty data found")
         return None
     return all_data
 
@@ -50,3 +59,8 @@ def df_to_xlsx(df, directory_path, file_name):
     os.makedirs(directory_path, exist_ok=True)
     file_name = f"{file_name}.xlsx" if not file_name.endswith('.xlsx') else file_name
     df.to_excel(os.path.join(directory_path, file_name), index=False)
+
+def df_to_csv(df, directory_path, file_name):
+    os.makedirs(directory_path, exist_ok=True)
+    file_name = f"{file_name}.csv" if not file_name.endswith('.csv') else file_name
+    df.to_csv(os.path.join(directory_path, file_name), index=False)
